eb8b37bacdeefcde1b04a01b27d03c09d01cdf86
Timothée Lottaz <timozattol@gmail.com>
2015-11-22 18:58:23 +0100

    Snake case for variables

--- main.py
+++ main.py
@@ -3,27 +3,27 @@
 from PIL import Image, ImageDraw
 from random import randint
 
-def rectangles_cut(imSize, rectCount):
+def rectangles_cut(im_size, rect_count):
     """ Returns a list of coordinates of rectangles that subdivide an image of
-    size imSize, with rectangleCount the number of rectangles in each
+    size im_size, with rectangleCount the number of rectangles in each
     dimension (X & Y axis) """
     rectangles = []
 
     # X and Y size of rectangles
-    rectSize = \
+    rect_size = \
     (
-        int(imSize[0] / rectCount[0]),
-        int(imSize[1] / rectCount[1])
+        int(im_size[0] / rect_count[0]),
+        int(im_size[1] / rect_count[1])
     )
 
     # Construct the list of rectangles
-    for i in range(0, rectCount[0]):
-        for j in range(0, rectCount[1]):
+    for i in range(0, rect_count[0]):
+        for j in range(0, rect_count[1]):
             rect = [
-                (i * rectSize[0], j * rectSize[1]),
-                ((i + 1) * rectSize[0], j * rectSize[1]),
-                ((i + 1) * rectSize[0], (j + 1) * rectSize[1]),
-                (i * rectSize[0], (j + 1) * rectSize[1])
+                (i * rect_size[0], j * rect_size[1]),
+                ((i + 1) * rect_size[0], j * rect_size[1]),
+                ((i + 1) * rect_size[0], (j + 1) * rect_size[1]),
+                (i * rect_size[0], (j + 1) * rect_size[1])
             ]
 
             rectangles.append(rect)
