b5acce403c58cc39b51b164781e4535eac2ac126
Timothée Lottaz <timozattol@gmail.com>
2015-12-23 12:15:22 +0100

    Create color palettes

--- main.py
+++ main.py
@@ -3,25 +3,23 @@
 from PIL import Image, ImageDraw
 from random import seed, randint
 from polylattice import PolyLattice
+from colors import palettes
 
 def main():
     seed('42')
 
     im = Image.new("RGB", [1200, 800], 0)
-
     image_draw = ImageDraw.Draw(im)
-
     polylattice = PolyLattice(im.size, (12, 8))
 
     polylattice.initialise()
-
     polylattice.mutate(40)
     polylattice.gradient_colors_random_direction(
-        (168, 223, 32),
-        (106, 32, 159)
+        palettes['pastel_bright'][0],
+        palettes['pastel_bright'][3]
     )
-    polylattice.draw(image_draw)
 
+    polylattice.draw(image_draw)
     im.show()
 
 if __name__ == '__main__':
