dbd80b8ef75261eb683cecd173d57d3b15304b84
Timothée Lottaz <timozattol@gmail.com>
2015-12-24 17:28:09 +0100

    Gnome's wallpaper is updated, comment main.py

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 #!/usr/bin/python3
 
 from PIL import Image, ImageDraw
-from random import seed, randint
+from random import seed, randint, sample
 from polylattice import PolyLattice
 from colors import palettes
+from os import getcwd
+
+import subprocess
 
 def main():
-    seed('42')
+    # Set seed to control randomness
+    # seed('42')
 
-    im = Image.new("RGB", [1200, 800], 0)
+    # Choose two colors from the palette
+    colors = sample(palettes['pastel_forest'], 2)
+
+    # Create an image of the size of the screen
+    im = Image.new("RGB", [1600, 900], 0)
     image_draw = ImageDraw.Draw(im)
-    polylattice = PolyLattice(im.size, (12, 8))
 
+    # Initialise a PolyLattice
+    polylattice = PolyLattice(im.size, (16, 9))
     polylattice.initialise()
-    polylattice.mutate(40)
-    polylattice.gradient_colors_random_direction(
-        palettes['pastel_bright'][0],
-        palettes['pastel_bright'][3]
-    )
 
+    # Mutate PolyLattice and apply random gradient of colors
+    polylattice.mutate(40)
+    polylattice.gradient_colors_random_direction(colors[0], colors[1])
+
+    # Draw the polylattice on the image
     polylattice.draw(image_draw)
-    im.show()
+
+    # Delete eventual previous renders
+    subprocess.call(["rm", "renders/wallpaper.jpg"], stderr=subprocess.DEVNULL)
+
+    # Save image in renders
+    im.save("renders/wallpaper.jpg")
+
+    # Update wallpaper
+    wd = getcwd()
+    subprocess.call(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file://" + wd + "/renders/wallpaper.jpg"])
 
 if __name__ == '__main__':
     main()
