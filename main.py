4f5df17fb14a3c0cd1bf336a5dba5a4db08ffa0b
Timothée Lottaz <timozattol@gmail.com>
2015-12-24 17:51:29 +0100

    Create configuration variables

--- main.py
+++ main.py
@@ -9,36 +9,46 @@ from os import getcwd
 import subprocess
 
 def main():
+    ## Configurations ##
+    palette = 'pastel_forest'
+    screen_size = (1600, 900)
+    chunk_size = (100, 100) # screen_size / chunk_size have to be integer
+    mutation_intensity = 40
+    render_path = "renders/wallpaper.jpg"
+
+
     # Set seed to control randomness
     # seed('42')
 
-    # Choose two colors from the palette
-    colors = sample(palettes['pastel_forest'], 2)
-
     # Create an image of the size of the screen
-    im = Image.new("RGB", [1600, 900], 0)
+    im = Image.new("RGB", screen_size, 0)
     image_draw = ImageDraw.Draw(im)
 
     # Initialise a PolyLattice
-    polylattice = PolyLattice(im.size, (16, 9))
+    poly_size_x = int(screen_size[0] / chunk_size[0])
+    poly_size_y = int(screen_size[1] / chunk_size[1])
+    polylattice = PolyLattice(im.size, (poly_size_x, poly_size_y))
     polylattice.initialise()
 
+    # Choose two colors from the palette
+    colors = sample(palettes[palette], 2)
+
     # Mutate PolyLattice and apply random gradient of colors
-    polylattice.mutate(40)
+    polylattice.mutate(mutation_intensity)
     polylattice.gradient_colors_random_direction(colors[0], colors[1])
 
     # Draw the polylattice on the image
     polylattice.draw(image_draw)
 
     # Delete eventual previous renders
-    subprocess.call(["rm", "renders/wallpaper.jpg"], stderr=subprocess.DEVNULL)
+    subprocess.call(["rm", render_path], stderr=subprocess.DEVNULL)
 
     # Save image in renders
-    im.save("renders/wallpaper.jpg")
+    im.save(render_path)
 
     # Update wallpaper
     wd = getcwd()
-    subprocess.call(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file://" + wd + "/renders/wallpaper.jpg"])
+    subprocess.call(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", "".join(["file://", wd, "/", render_path])])
 
 if __name__ == '__main__':
     main()
