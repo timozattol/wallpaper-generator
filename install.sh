7e7536a5e084a43a037ff4ab938e5e5150080a34
Timothée Lottaz <timozattol@gmail.com>
2015-12-26 13:49:51 +0100

    Prompt in install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ SCRIPTDIR=$(dirname "$SCRIPTPATH")
 
 # Run main.py every 30 minutes
 echo "0,30 * * * * $SCRIPTDIR/main.py" | crontab -
+
+echo "Crontab sucessfully added! Wallpaper will change every 30 minutes. To uninstall: \"crontab -r\""
