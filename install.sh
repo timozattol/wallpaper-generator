db696443c8247d7f0281e962818d15b21ddba5e2
Timothée Lottaz <timozattol@gmail.com>
2015-12-26 12:59:21 +0100

    Create install.sh
    Installs a crontab that runs main.py every 30 minutes

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Find path of directory
+SCRIPTPATH=$(readlink -f "$0")
+SCRIPTDIR=$(dirname "$SCRIPTPATH")
+
+# Run main.py every 30 minutes
+echo "0,30 * * * * $SCRIPTDIR/main.py" | crontab -
