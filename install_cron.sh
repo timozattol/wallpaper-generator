0454dbf6d2a31d68355d88888dfc209e6c1ac503
Timothée Lottaz <timozattol@gmail.com>
2016-07-15 16:00:02 +0200

    Fix #9, add licence

--- install_cron.sh
+++ install_cron.sh
@@ -4,7 +4,12 @@
 SCRIPTPATH=$(readlink -f "$0")
 SCRIPTDIR=$(dirname "$SCRIPTPATH")
 
+# Find resolution dynamically
+resolution=$(xdpyinfo | awk '/dimensions/{print $2}')
+
+echo "Using resolution: $resolution"
+
 # Run main.py every 30 minutes
-echo "0,30 * * * * $SCRIPTDIR/main.py" | crontab -
+echo "0,30 * * * * $SCRIPTDIR/main.py" "$resolution" | crontab -
 
 echo "Crontab sucessfully added! Wallpaper will change every 30 minutes. To uninstall: \"crontab -r\""
--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
 
+"""
+Simple python wallpaper generator.
+
+Usage: main.py [WIDTHxHEIGHT].
+Code and examples here: https://github.com/timozattol/wallpaper-generator
+"""
+
+__author__ = "Timothée Lottaz"
+__licence__ = "MIT"
+__email__ = "timozattol@gmail.com"
+__status__ = "Prototype"
+
 from os import path, makedirs
 from math import ceil
-import subprocess
+import subprocess, sys
 from PIL import Image, ImageDraw
 from random import sample
 from polylattice import PolyLattice
 from colors import palettes
+
 
 def main():
 
     ## Configurations ##
     palette = 'pastel_forest'
     mutation_intensity = 30
+    default_resolution = [1920, 1080]
 
     # Polygons have a fixed size in px. Higher resolution = more polygons
     poly_sizes = (120, 100)
-
 
     ## Paths ##
     file_path = path.realpath(__file__)
@@ -28,29 +41,34 @@ def main():
 
     render_file = render_folder + "/wallpaper.jpg"
 
-    # Get resolution dynamically
-    cmd1 = ['xrandr']
-    cmd2 = ['grep', '*']
+    # Get resolution from program arguments, or use default resolution
+    resolution = default_resolution
 
-    process = subprocess.Popen(cmd1, stdout=subprocess.PIPE)
-    process2 = subprocess.Popen(cmd2, stdin=process.stdout, stdout=subprocess.PIPE)
-    process.stdout.close()
+    if len(sys.argv) >= 2:
+        try:
+            # Try parsing resolution from argv[1]
+            res_parse = sys.argv[1].split("x")
 
-    binary_res, _ = process2.communicate()
-    binary_res = binary_res.split()[0]
-    resolution = binary_res.decode("utf-8").split('x')
+            if len(res_parse) != 2:
+                raise ValueError()
 
-    width = int(resolution[0])
-    height = int(resolution[1])
-    screen_size = (width, height)
+            res_parse = [int(x) for x in res_parse]
+
+            if any(x < 0 for x in res_parse):
+                raise ValueError()
+
+            resolution = res_parse
+
+        except ValueError:
+            sys.stderr.write('Resolution given in arguments must be written like "1920x1080". Using default resolution...')
 
     # Create an image of the size of the screen
-    im = Image.new("RGB", screen_size, 0)
+    im = Image.new("RGB", resolution, 0)
     image_draw = ImageDraw.Draw(im)
 
     # Initialise a PolyLattice
-    poly_count_x = (screen_size[0] / poly_sizes[0])
-    poly_count_y = (screen_size[1] / poly_sizes[1])
+    poly_count_x = (resolution[0] / poly_sizes[0])
+    poly_count_y = (resolution[1] / poly_sizes[1])
 
     # Last polygons might be partly overflowing the image
     polylattice = PolyLattice(
